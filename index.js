1c7843357a46abb6d52f52ce3c825bb5e03e92ba
glckfndr <obulan65@gmail.com>
2024-09-29 01:23:11 +0300

    feat: add todo list view

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+
+const connectionURL = "mongodb://localhost:27017/todoDB)";
+
+mongoose
+  .connect(connectionURL)
+  .then(() => {
+    console.log("Connection to mongoDB is successful!");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
 const port = 8000;
 app.set("view engine", "ejs");
+
+app.get("/", (req, res, next) => {
+  try {
+    res.render("index");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
